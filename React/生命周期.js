e6bd0a0c50ab27f79bc7c3010af1602fbb25ff75
macxu <adherentxu@gmail.com>
2017-09-09 15:22:00 +0800

    生命周期改

--- React/生命周期.js
+++ React/生命周期.js
@@ -4,3 +4,36 @@
     2.当组件接受新的数据时，即组件更新时。
 */
 
+//组件挂载
+import React, { Component, PropTypes } from 'react';
+
+class App extends Component {
+    //这两个静态属性，可以在类外面访问它们，比如：
+    //App.propTypes 和 App.defaultProps.
+    static propTypes = {
+        //props类型检查器
+    };
+
+    static defaultProps = {
+        //默认类型
+    };
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        };
+    }
+    componentWillMount() {
+        //在render方法之 前 执行
+    }
+
+    componentDidMount() {
+        //在render方法之 后 执行
+    }
+
+    render(){
+        return <div>This is a demo</div>
+    }
+}
